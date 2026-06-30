02f220de295be8e083579d47e70b9378fc4cbccd
istomanin <istomanin@gmail.com>
2026-06-30 15:25:33 +0600

    added character creation and clone data from SO
    При запуске игры создается копия данных из SO. Добавлена функция пересчета характеристик

--- Assets/ChooseProject/Scripts/Character/CharacterManager.cs
+++ Assets/ChooseProject/Scripts/Character/CharacterManager.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 public class CharacterManager : MonoBehaviour
 {
     private List<StatModifier> modifiers = new();
+    [SerializeField]
+    private CharacterData currentCharacterData;
+
+    private CharacterStats baseStats;
+
+    private CharacterStats finalStats;
 
 
+    private void Start()
+    {
+        baseStats = CreateStats(currentCharacterData);
+
+        RecalculateStats();
+    }
 
     public void AddRandomModifier()
     {
-        StatModifier modifier = new StatModifier();
+        StatModifier modifier = new()
+        {
+            StatType = (StatType)Random.Range(0, 4),
 
-        modifier.StatType = (StatType)Random.Range(0, 4);
-
-        modifier.Value = Random.Range(10, 31);
+            Value = Random.Range(10, 31)
+        };
 
         modifiers.Add(modifier);
+
+        RecalculateStats();
     }
 
     public void RemoveLastModifier()
@@ -24,5 +39,44 @@ public class CharacterManager : MonoBehaviour
             return;
 
         modifiers.RemoveAt(modifiers.Count - 1);
+        RecalculateStats();
+    }
+
+    private CharacterStats CreateStats(CharacterData data)
+    {
+        return new CharacterStats()
+        {
+            Health = data.Health,
+            Attack = data.Attack,
+            Defense = data.Defense,
+            Speed = data.Speed
+        };
+    }
+
+    private void RecalculateStats()
+    {
+        finalStats = (CharacterStats)baseStats.Clone();
+
+        foreach (StatModifier modifier in modifiers)
+        {
+            switch (modifier.StatType)
+            {
+                case StatType.Health:
+                    finalStats.Health += modifier.Value;
+                    break;
+
+                case StatType.Attack:
+                    finalStats.Attack += modifier.Value;
+                    break;
+
+                case StatType.Defense:
+                    finalStats.Defense += modifier.Value;
+                    break;
+
+                case StatType.Speed:
+                    finalStats.Speed += modifier.Value;
+                    break;
+            }
+        }
     }
 }
